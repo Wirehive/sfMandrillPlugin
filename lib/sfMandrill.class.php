a07e5016f1fe52da605727c4d28404624d003859
Robin Corps <robin@ngse.co.uk>
2014-09-23 17:39:39 +0100

    Updated wrapper

--- lib/sfMandrill.class.php
+++ lib/sfMandrill.class.php
@@ -1,9 +1,10 @@
 <?php
 
+
 /**
  * A Symfony 1.4 plugin wrapper for the Mandrill API library.
  *
- * @author Robin Corps <robin@ngse.co.uk>
+ * @author  Robin Corps <robin@ngse.co.uk>
  * @version 0.1
  * @package sfMandrillPlugin
  */
@@ -22,29 +23,5 @@ class sfMandrill extends Mandrill
     }
 
     parent::__construct($apikey);
-  }
-
-
-  /**
-   * Calls a Mandrill API method.
-   *
-   * Returns the result from that call or, if there was an error,
-   * throws an exception.
-   *
-   * @param string $method
-   * @param array|null $params
-   * @return array
-   * @throws Mandrill_Error
-   */
-  public function __get($method)
-  {
-    if (isset($this->$method))
-    {
-      return $this->$method;
-    }
-    else
-    {
-      throw new Mandrill_Error('Method not implemented');
-    }
   }
 } 
